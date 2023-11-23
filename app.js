495bff2d33c8f3f1200ab596165850c69c5d3a63
Saumya <bansalgudiya350@gmail.com>
2023-11-23 23:06:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+let sach=true;
+console.log(sach);
+
+//typecasting->dharamparivartan
+let ans=sach==1;
+console.log(ans)
+
+// 1.general equality->==
+// 2.Strict equality->===
+
+let ans2=sach===1;
+console.log(ans2);
+
+let a=10;
+if (a=='10'){
+    console.log("matched")
+}
+else{
+    console.log("not matched")
+}
+
+// let age=18;
+// if (age<18){
+//     console.log("your entry in the club is restricted");
+// }
+// else if (age>=18 && age<=25){
+//     console.log("your entry is allowed but drinking and romance is prohibited")
+// }
+// else{
+//     console.log("everything is allowed")
+// }
+
+
+let marks=100;
+let attendance=88;
+if (attendance>75||mark>33){
+    console.log('pass')
+}
+else{
+    console.log('fail')
+}
+
+//! ->not
+let answer=!true==false
+console.log(answer)
